317e0ba3369ad7879bfd14791bb244d18f920567
l0mono <matsudayuki030321@gmail.com>
2023-08-14 11:14:35 +0900

    ~36:50

--- apple/main.js
+++ apple/main.js
@@ -1,4 +1,45 @@
 enchant();
 window.onload = function () {
-    console.log("Hey, enchant.js!");
+    
+    var core = new Core(640,640);
+    core.preload("cat.png");
+    core.fps = 30;
+    core.onload = function () {
+
+        const TIME = 10;
+
+        var cat = new Sprite(100,100);
+        cat.image = core.assets["cat.png"];
+        cat.x = 270;
+        cat.y = 540;
+        cat.scaleX = 0.5;
+        cat.scaleY = 0.5;
+
+        cat.frame = 0;
+        cat.on("enterframe",function(){
+            if(core.input.left){
+                this.frame = this.age%2;
+                this.x -= 5;
+                cat.scaleX = -0.5;
+            }
+            if(core.input.right){
+                this.frame = this.age%2;
+                this.x += 5;
+                cat.scaleX = 0.5;
+            }
+        });
+        core.rootScene.addChild(cat);
+
+        var time = new Label();
+        time.x = 10;
+        time.y = 10;
+        time.font = "20px Hirageno";
+        time.text = "Time: " ;
+        core.rootScene.addChild(time);
+
+        core.rootScene.on("enterframe", function () {
+            time.text = "Time: "+((((TIME * core.fps) - core.frame)) / core.fps).toFixed(0);
+        })
+    }
+    core.start();
 }
